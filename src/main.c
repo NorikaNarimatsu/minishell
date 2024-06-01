46ee772ff29af0e7e49ac94e6b52a7775ec0e4d2
Norika Narimatsu <nnarimat@f0r4s22.codam.nl>
2024-06-01 11:24:43 +0200

    adding fork for command and creating the tester.sh

--- src/main.c
+++ src/main.c
@@ -6,11 +6,36 @@
 /*   By: nnarimat <nnarimat@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/26 12:36:23 by nnarimat          #+#    #+#             */
-/*   Updated: 2024/06/01 11:18:57 by nnarimat         ###   ########.fr       */
+/*   Updated: 2024/06/01 11:23:09 by nnarimat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
+
+int	ft_pipex(char *line, char **env)
+{
+	pid_t	pid;
+	int		status;
+	char	*argv[] = {line, NULL};
+
+	pid = fork();
+	if (pid < 0)
+	{
+		ft_putstr_fd("pid error\n", 2);
+		exit(EXIT_FAILURE);
+	}
+	else if (pid == 0)
+	{
+		execve(line, argv, env);
+		ft_putstr_fd("evecve error\n", 2);
+		exit(EXIT_FAILURE);
+	}
+	else
+	{
+		wait(&status);
+		return (WEXITSTATUS(status));
+	}
+}
 
 int	main(int argc, char **argv, char **env)
 {
@@ -18,7 +43,7 @@ int	main(int argc, char **argv, char **env)
 
 	(void) argc;
 	(void) argv;
-	(void) env;
+	rl_outstream = stderr;
 	while (1)
 	{
 		line = readline("minishell$ ");
@@ -26,6 +51,7 @@ int	main(int argc, char **argv, char **env)
 			break ;
 		if (*line)
 			add_history(line);
+		ft_pipex(line, env);
 		free(line);
 	}
 	exit(0);
