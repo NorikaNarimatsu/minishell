4ab0e91683866c912d7c6b106d733271645675d7
Norika Narimatsu <nnarimat@f0r4s22.codam.nl>
2024-06-01 19:12:21 +0200

    adding path check from pipex

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: nnarimat <nnarimat@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/09 13:09:53 by nnarimat          #+#    #+#             */
-/*   Updated: 2024/05/31 11:53:45 by nnarimat         ###   ########.fr       */
+/*   Updated: 2024/06/01 18:52:39 by nnarimat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,5 +23,7 @@
 # include <fcntl.h>
 # include <readline/readline.h>
 # include <readline/history.h>
+# include <limits.h>
+# include <string.h>
 
 #endif
--- src/main.c
+++ src/main.c
@@ -6,17 +6,60 @@
 /*   By: nnarimat <nnarimat@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/26 12:36:23 by nnarimat          #+#    #+#             */
-/*   Updated: 2024/06/01 11:23:09 by nnarimat         ###   ########.fr       */
+/*   Updated: 2024/06/01 19:00:52 by nnarimat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-int	ft_pipex(char *line, char **env)
+void	error_exit(char *location, char *msg, int status)
+{
+	dprintf(STDERR_FILENO, "minishell: %s: %s\n", location, msg); //to do
+	exit(status);
+}
+
+char	*search_path(char *filename)
+{
+	char	path[PATH_MAX];
+	char	*value;
+	char	*end;
+
+	value = getenv("PATH"); // todo
+	while (*value)
+	{
+		bzero(path, PATH_MAX);
+		end = ft_strchr(value, ':');
+		if (end)
+			strncpy(path, value, end - value); //todo
+		else
+			ft_strlcpy(path, value, PATH_MAX);
+		ft_strlcat(path, "/", PATH_MAX);
+		ft_strlcat(path, filename, PATH_MAX);
+		if (access(path, X_OK) == 0)
+		{
+			char	*dup;
+
+			dup = ft_strdup(path);
+			if (dup == NULL)
+			{
+				ft_putstr_fd("pid error\n", 2);
+				exit(EXIT_FAILURE);
+			}
+			return (dup);
+		}
+		if (end == NULL)
+			return (NULL);
+		value = end + 1;
+	}
+	return (NULL);
+}
+
+int	ft_interpret(char *line, char **env)
 {
 	pid_t	pid;
 	int		status;
-	char	*argv[] = {line, NULL};
+	char *argv[] = {line, NULL};
+	char	*path = argv[0];
 
 	pid = fork();
 	if (pid < 0)
@@ -26,7 +69,13 @@ int	ft_pipex(char *line, char **env)
 	}
 	else if (pid == 0)
 	{
-		execve(line, argv, env);
+		if (ft_strchr(path, '/') == NULL)
+			path = search_path(path);
+		if (path == NULL)
+			error_exit(argv[0], "command not found", 127);
+		if (access(path, F_OK) < 0)
+			error_exit(argv[0], "command not found", 127);
+		execve(path, argv, env);
 		ft_putstr_fd("evecve error\n", 2);
 		exit(EXIT_FAILURE);
 	}
@@ -51,7 +100,7 @@ int	main(int argc, char **argv, char **env)
 			break ;
 		if (*line)
 			add_history(line);
-		ft_pipex(line, env);
+		ft_interpret(line, env);
 		free(line);
 	}
 	exit(0);
