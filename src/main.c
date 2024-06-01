38379c845f23eca2da009f864aad603568ed1838
Norika Narimatsu <nnarimat@f0r4s22.codam.nl>
2024-06-01 11:19:57 +0200

    setting up readline loop with prompt

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: nnarimat <nnarimat@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/26 12:36:23 by nnarimat          #+#    #+#             */
-/*   Updated: 2024/06/01 11:15:41 by nnarimat         ###   ########.fr       */
+/*   Updated: 2024/06/01 11:18:57 by nnarimat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,19 @@
 
 int	main(int argc, char **argv, char **env)
 {
+	char	*line;
+
 	(void) argc;
 	(void) argv;
 	(void) env;
-
-	ft_printf("Environment Test\n");
+	while (1)
+	{
+		line = readline("minishell$ ");
+		if (line == NULL)
+			break ;
+		if (*line)
+			add_history(line);
+		free(line);
+	}
+	exit(0);
 }
